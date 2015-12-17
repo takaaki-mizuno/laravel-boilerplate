355d5320ab9a77e491135b223a5d9098ae809de5
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2015-12-17 16:36:17 +0800

    Fixed Required Libraries

---
+++ app/Models/LocaleStorableBase.php
@@ -0,0 +1,24 @@
+<?php namespace App\Models;
+
+/**
+ * App\Models\LocaleStorableBase
+ *
+ * @property string         $locale
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\LocaleStorableBase whereLocale($value)
+ */
+
+class LocaleStorableBase extends Base
+{
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    public function setLocale($locale)
+    {
+        $this->locale = strtolower($locale);
+        $this->save();
+    }
+
+}

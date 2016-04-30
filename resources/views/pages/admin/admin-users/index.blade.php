ba508f5e963085639f7d8f4c1a3525d35ecb1eea
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2016-04-30 18:04:40 +0800

    Admin User Fixed

--- resources/views/pages/admin/admin-users/index.blade.php
+++ resources/views/pages/admin/admin-users/index.blade.php
@@ -10,32 +10,46 @@
 @stop
 
 @section('title')
-{{ \Config::get('site.name') }} | Admin | Admin Users
+    {{ \Config::get('site.name') }} | Admin | Admin Users
 @stop
 
 @section('header')
-Admin Users
+    Admin Users
 @stop
 
 @section('content')
-    <div class="box-body no-padding">
-        <table class="table">
-            <tr>
-                <th style="width: 10px">#</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th style="width: 40px"></th>
-            </tr>
-            @foreach( $adminUsers as $adminUser )
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title">
+                <p class="text-right">
+                    <a href="{!! URL::action('Admin\SiteConfigurationController@create') !!}"
+                       class="btn btn-block btn-primary btn-sm">@lang('admin.pages.common.buttons.create')</a>
+                </p>
+            </h3>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table">
                 <tr>
-                    <td>{{ $adminUser->id }}</td>
-                    <td>{{ $adminUser->name }}</td>
-                    <td>{{ $adminUser->email }}</td>
-                    <td>
-                        <a href="{!! URL::action('Admin\AdminUserController@show', [$adminUser->id]) !!}" class="btn btn-block btn-primary">Edit</a>
-                    </td>
+                    <th style="width: 10px">#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th style="width: 40px"></th>
                 </tr>
-            @endforeach
-        </table>
+                @foreach( $adminUsers as $adminUser )
+                    <tr>
+                        <td>{{ $adminUser->id }}</td>
+                        <td>{{ $adminUser->name }}</td>
+                        <td>{{ $adminUser->email }}</td>
+                        <td>
+                            <a href="{!! URL::action('Admin\AdminUserController@show', [$adminUser->id]) !!}"
+                               class="btn btn-block btn-primary">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+        <div class="box-footer">
+            @include('shared.admin.pagination')
+        </div>
     </div>
 @stop

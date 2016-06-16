fb6e585a93796498ecefa3c16c2d9f0171be6c16
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2016-06-16 18:35:57 +0800

    Add SecurePath Redirection Middleware

---
+++ app/Http/Middleware/SecurePath.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SecurePath
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (\App::environment('production') && !\Request::secure()) {
+            // The environment is production
+            return \Redirect::secure(\Request::path());
+        }
+
+        return $next($request);
+    }
+}

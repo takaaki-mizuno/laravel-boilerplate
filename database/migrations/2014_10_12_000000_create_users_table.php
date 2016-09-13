0edf6ab339c5487d5e08cdd16ce178299f5a4508
Son Bui <sonbv00@gmail.com>
2016-09-13 16:52:16 +0700

    Fix #25: Signup with an exist email address - Add validate email unique in signup request - Modify email field to be unique in databasea schema - Add some test case

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('password', 60);
 
             $table->string('locale')->default('');

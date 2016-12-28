dc0cd8a27465a7e69891506339260022e7373ead
thanhnt <tatthanh.dev@gmail.com>
2016-12-28 10:57:13 +0700

    #1: remove some code not relate

--- resources/views/layouts/admin/left_navigation.blade.php
+++ resources/views/layouts/admin/left_navigation.blade.php
@@ -34,9 +34,6 @@
             <li @if( $menu=='user_notifications') class="active" @endif ><a href="{!! URL::action('Admin\UserNotificationController@index') !!}"><i class="fa fa-bell"></i> <span>UserNotifications</span></a></li>
             <li @if( $menu=='admin_user_notifications') class="active" @endif ><a href="{!! URL::action('Admin\AdminUserNotificationController@index') !!}"><i class="fa fa-bell-o"></i> <span>AdminUserNotifications</span></a></li>
             <li @if( $menu=='images') class="active" @endif ><a href="{!! URL::action('Admin\ImageController@index') !!}"><i class="fa fa-file-image-o"></i> <span>Images</span></a></li>
-            <li @if( $menu=='customers') class="active" @endif ><a href="{!! action('Admin\CustomerController@index') !!}"><i class="fa fa-users"></i> <span>Customers</span></a></li>
-            <li @if( $menu=='employees') class="active" @endif ><a href="{!! action('Admin\EmployeeController@index') !!}"><i class="fa fa-users"></i> <span>Employees</span></a></li>
-            <li @if( $menu=='products') class="active" @endif ><a href="{!! action('Admin\ProductController@index') !!}"><i class="fa fa-users"></i> <span>Products</span></a></li>
             <!-- %%SIDEMENU%% -->
         </ul>
     </section>

f839fb286798f3344df435d327c7900f08e497c2
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-01-08 12:03:05 +0800

    Fixed Admin SignIn Redirection

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
             return redirect()->action('Admin\AuthController@getSignIn');
         }
 
-        return \RedirectHelper::intended('Admin\IndexController@index', $this->adminUserService->getGuardName());
+        return \RedirectHelper::intended(action('Admin\IndexController@index'), $this->adminUserService->getGuardName());
     }
 
     public function postSignOut()
